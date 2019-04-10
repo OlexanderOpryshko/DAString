2b208b9c7fba4e35dbb03b8b9aceba102fcfa596
Opryshko Olexander <opryshkoolexander@gmail.com>
2019-04-10 08:43:10 +0300

    Add function da_atoi, that convert string of digit to integer number. Some minor changes

--- DAString.c
+++ DAString.c
@@ -1,13 +1,41 @@
 #include "DAString.h"
+#include <stdbool.h>
 
-#define NULL 0
+#define DA_NULL 0
 
 int da_strlen(char * array)
 {
 	int i = 0;
 	
-	while (array[i] != NULL)
+	while (array[i])
 		i++;
 	
 	return i;
+}
+
+int da_atoi(char * array)
+{
+	int number = 0;
+	int length = da_strlen(array);
+	int k = 1;
+
+	for (int i = length - 1; i >= 0; --i)
+	{
+		if (array[i] == '-')
+		{
+			number *= -1;
+			break;
+		}
+		else if (array[i] == ' ')
+		{
+			continue;
+		}
+		else if (array[i] > '9' || array[i] < '0')
+			return 0;
+
+		number += (array[i] - '0') * k;
+		k *= 10;
+	}
+
+	return number;
 }
--- DAString.h
+++ DAString.h
@@ -4,9 +4,15 @@
 int da_strlen(char * array);
 /*
 **
-** Function that returns length of string
+**	Function that returns length of string
 **
 */
 
+int da_atoi(char * array);
+/*
+**
+**	Function for convert string of digit to integer number
+**
+*/
 
 #endif
